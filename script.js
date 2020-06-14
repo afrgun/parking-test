e0342c834219b5cc7072d5b84c80256b083512dc
afrilagunadi <afrila.aries@gmail.com>
2020-06-14 17:52:14 +0700

    function create slot"

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ function createSlot() {
 	var slotPark = prompt("Create parking lot of size n:");
 	if (slotPark == null || slotPark == "") {
 	  txt = "Canceled";
-	  document.getElementById("listPark").innerHTML = txt;
 	} else {
 	  txt = slotPark;
 	  for(var i = 0; i < slotPark; i++){
 		  resultPark.push('park KA-01-HH-12'+i);
     }
     
-    var str = '<ul>';
+    txt = '<ul>';
     resultPark.forEach(function(slide) {
-      str += '<li>'+ slide + '</li>';
+      txt += '<li>'+ slide + '</li>';
     }); 
-    str += '</ul>';
-
-		document.getElementById("listPark").innerHTML = str;
-	}
+    txt += '</ul>';
+  }
+  
+  document.getElementById("listPark").innerHTML = txt;
 }
