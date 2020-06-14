38c16cc80f1c026758b18d8443fb458c32176a8e
afrilagunadi <afrila.aries@gmail.com>
2020-06-14 18:58:52 +0700

    function parkIn

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+var txt, slotAvailable, listParking;
+var resultPark = [];
+
 function createSlot() {
-	var txt;
-	var resultPark = [];
-	var slotPark = prompt("Create parking lot of size n:");
+  var slotPark = prompt("Create parking lot of size n:");
 	if (slotPark == null || slotPark == "") {
 	  txt = "Canceled";
 	} else {
-	  txt = slotPark;
-	  for(var i = 0; i < slotPark; i++){
-		  resultPark.push('park KA-01-HH-12'+i);
-    }
-    
-    txt = '<ul>';
-    resultPark.forEach(function(slide) {
-      txt += '<li>'+ slide + '</li>';
-    }); 
-    txt += '</ul>';
-  }
+    slotAvailable = slotPark;
+	  txt = 'Created parking lot with ' + slotPark + ' slots';
+	}
   
-  document.getElementById("listPark").innerHTML = txt;
+  document.getElementById("slotParking").innerHTML = txt;
+}
+
+function parkIn(){
+  var carNumber = prompt("Vehicle number:");
+
+  if (carNumber == null || carNumber == "") {
+    txt = "Canceled";
+  } else {
+    if(slotAvailable == 0){
+      return alert('Sorry, parking lot is full');
+    }
+    resultPark.push("park " + carNumber);
+  }
+
+  slotAvailable -= 1;
+
+  checkSlot();
+
+  document.getElementById("listParking").innerHTML = listParking;
+  document.getElementById("slotParking").innerHTML = slotAvailable + " Slots Avalaible";
+}
+
+function checkSlot(){
+  listParking = '<ul>';
+  resultPark.forEach(function(car) {
+    listParking += '<li>'+ car + '</li>';
+  }); 
+  listParking += '</ul>';
+
+  return listParking;
 }
