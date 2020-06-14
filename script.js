6524f22fcaaed752058b818e221e33f3a2046a35
afrilagunadi <afrila.aries@gmail.com>
2020-06-14 22:45:49 +0700

    function parking out

--- script.js
+++ script.js
@@ -14,31 +14,75 @@ function createSlot() {
 }
 
 function parkIn(){
+  if(slotAvailable == 0){
+    return alert('Sorry, parking lot is full');
+  }
+
   var carNumber = prompt("Vehicle number:");
 
   if (carNumber == null || carNumber == "") {
     txt = "Canceled";
-  } else {
-    if(slotAvailable == 0){
-      return alert('Sorry, parking lot is full');
+  } else {  
+    var carColor = prompt('Vehicle color:');
+    if (carColor == null || carColor == "") {
+      return
     }
-    resultPark.push("park " + carNumber);
+
+    resultPark.push({
+      'vehicle_number': "park " + carNumber,
+      'vehicle_color': carColor
+    });
+    
   }
 
   slotAvailable -= 1;
 
   checkSlot();
-
-  document.getElementById("listParking").innerHTML = listParking;
   document.getElementById("slotParking").innerHTML = slotAvailable + " Slots Avalaible";
 }
+
 
 function checkSlot(){
   listParking = '<ul>';
   resultPark.forEach(function(car) {
-    listParking += '<li>'+ car + '</li>';
+    listParking += '<li>'+ car.vehicle_number + '</li>';
   }); 
   listParking += '</ul>';
 
-  return listParking;
+  return document.getElementById("listParking").innerHTML = listParking;
+}
+
+function parkOut(){
+  var charge;
+  var vehicleNo = prompt("Vehicle Number:");
+  if (vehicleNo == null || vehicleNo == "") {
+	  return
+  } else {
+    const list = resultPark.filter(x => x.vehicle_number === 'park ' + vehicleNo)
+    let index = resultPark.findIndex(x => x.vehicle_number === 'park ' + vehicleNo)
+
+
+    if(list.length > 0){
+      var hours = prompt('Hours:')
+      if (hours == null || hours == "") {
+        return
+      } else {
+        if(hours > 2){
+          charge = ( hours - 2 ) * 10 + 10;
+          alert("Charge $" + charge);
+        } else {
+          alert("Charge $10");
+        }
+
+        resultPark.splice(index, 1);
+      }
+    } else {
+      alert('Data not found');
+    }
+  }
+
+  slotAvailable += 1;
+
+  checkSlot();
+  document.getElementById("slotParking").innerHTML = slotAvailable + " Slots Avalaible";
 }
